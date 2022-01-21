a4e8aa287d7d9910eb5488815423ae075d21eb32
Zarb1N <60680932+Zarb1N@users.noreply.github.com>
2022-01-21 08:58:23 +0300

    Prices for hectares was replaced from index.html to separate js-file

---
+++ include/js/priceList.js
@@ -0,0 +1,18 @@
+const priceList = [
+  {
+    areaInterval: [0, 500],
+    pricePerHectare: 0,
+  },
+  {
+    areaInterval: [500, 5000],
+    pricePerHectare: 6,
+  },
+  {
+    areaInterval: [5000, 15000],
+    pricePerHectare: 4,
+  },
+  {
+    areaInterval: [15000, Infinity],
+    pricePerHectare: 2,
+  },
+]
